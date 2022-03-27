61e38a61bc07272c5c51963e6af65051ce9022b6
akizyfee <87452790+akizyfee@users.noreply.github.com>
2022-03-27 19:29:16 +0800

    add errorHandle

--- errorHandle.js
+++ errorHandle.js
@@ -1,18 +1,21 @@
-function errorHandle(res){
+function errorHandle(res, statusCode, message) {
+
     const headers = {
         'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
         'Content-Type': 'application/json'
-    };
-    res.writeHead(400, headers);
-    res.write(JSON.stringify(
-        {
-            "status": "false",
-            "message": "欄位未填寫正確，或無此 todo ID"
-        }
-    ));
+    }
+
+    res.writeHead(statusCode, headers)
+    res.write(JSON.stringify({
+      status: 'false',
+      message
+    }))
+    
     res.end();
 }
 
 module.exports = errorHandle;
+
+//使用時 errorHandle(res, 想要回傳的狀態碼, '想要回傳的錯誤提醒文字') 就可以囉~
