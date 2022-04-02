f48891ab7ddd04b5979e158616e97d4d37ec4ed9
hsinyun <ruby8941@gmail.com>
2022-04-02 14:43:39 +0800

    [postTodo] create postTodo

---
+++ postTodo.js
@@ -0,0 +1,31 @@
+const errorHandle = require('./errorHandle');
+const { v4: uuidv4 } = require('uuid');
+const headers = require('./headers');
+
+function postTodo(res, body, todos) {
+  try {
+    const data = JSON.parse(body);
+    if (!data.title) {
+      // title required
+      errorHandle(res, 400, 'data formart not correct or title is required');
+    } else {
+      const todo = {
+        id: uuidv4(),
+        title: data.title,
+      };
+      todos.push(todo);
+      res.writeHead(201, headers);
+      res.write(
+        JSON.stringify({
+          status: 'success',
+          todos,
+        }),
+      );
+      res.end();
+    }
+  } catch (err) {
+    errorHandle(res, 400, 'data formart not correct or title is required');
+  }
+}
+
+module.exports = postTodo;
